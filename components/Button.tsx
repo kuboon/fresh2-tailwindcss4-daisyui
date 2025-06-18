5e19fb567201081f6a9ea0ab0a526400c5d52030
Ohkubo KOHEI <o@kbn.one>
2025-06-18 11:41:37 +0900

    add daisyui5

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ export function Button(props: ButtonProps) {
   return (
     <button
       {...props}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
+      class="btn"
     />
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,12 @@
 import { useSignal } from "@preact/signals";
 import { define } from "../utils.ts";
 import Counter from "../islands/Counter.tsx";
+import { ThemeController } from "../components/ThemeController.tsx";
 
 export default define.page(function Home() {
   const count = useSignal(3);
 
-  return (
+  return <>
     <div class="px-4 py-8 mx-auto fresh-gradient">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
@@ -23,5 +24,6 @@ export default define.page(function Home() {
         <Counter count={count} />
       </div>
     </div>
-  );
+    <ThemeController />
+  </>;
 });
